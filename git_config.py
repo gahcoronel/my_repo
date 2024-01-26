54af449014e3de35e83694334e2b9293901204b5
Ariel Coronel <gahcoronel@gmail.com>
2024-01-26 20:21:19 -0300

    Este es mi primer commit

---
+++ git_config.py
@@ -0,0 +1,21 @@
+# Comandos
+
+# para verificar la instalación de Git
+> git --version             
+git version 2.43.0.windows.1
+
+# configurar nombre de usuario e email
+> git config --global user.name "Ariel Coronel"             
+> git config --global user.email "gahcoronel@gmail.com"     
+
+# verificar datos de configuración
+> git config --list                                                         
+...
+user.name=Ariel Coronel
+user.email=gahcoronel@gmail.com
+
+# verificar datos de configuración
+> git config user.name
+Ariel Coronel
+> git config user.email
+gahcoronel@gmail.com
